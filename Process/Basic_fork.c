c66ba0ac9eebab47ae83ca940fefe3de44732bdc
siddhant <siddhantmunjamkar23@gmail.com>
2026-02-20 23:20:24 +0530

    adding the basic_files

---
+++ Process/Basic_fork.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int global_var = 100;
+
+int main()
+{
+
+    int local_var = 20;
+
+    printf("Process ID: %d\n", getpid());
+    printf("Global var: %d\n", &global_var);
+    printf("Local var: %d\n", &local_var);
+
+    while (1)
+        ;
+    return 0;
+}
